d76c1fefeb6a66c01387ea0a635ebd2879b8dd78
zyn <429276924@qq.com>
2019-12-12 14:00:44 +0800

    修改AbstractLifeCycle线程安全问题

--- src/main/java/com/yundepot/oaa/common/AbstractLifeCycle.java
+++ src/main/java/com/yundepot/oaa/common/AbstractLifeCycle.java
@@ -2,34 +2,32 @@ package com.yundepot.oaa.common;
 
 import com.yundepot.oaa.exception.LifeCycleException;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * @author zhaiyanan
  * @date 2019/5/31 06:25
  */
 public abstract class AbstractLifeCycle implements LifeCycle {
 
-    private volatile boolean isStarted = false;
+    private final AtomicBoolean isStarted = new AtomicBoolean(false);
 
     @Override
     public void start() {
-        if (!isStarted) {
-            isStarted = true;
-            return;
+        if (!isStarted.compareAndSet(false, true)) {
+            throw new LifeCycleException("this component has started");
         }
-        throw new LifeCycleException("this component has started");
     }
 
     @Override
     public void shutdown() {
-        if (isStarted) {
-            isStarted = false;
-            return;
+        if (!isStarted.compareAndSet(true, false)) {
+            throw new LifeCycleException("this component has closed");
         }
-        throw new LifeCycleException("this component has closed");
     }
 
     @Override
     public boolean isStarted() {
-        return isStarted;
+        return isStarted.get();
     }
 }
